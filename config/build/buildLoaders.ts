6d898b83c3dbe30570aeaa711da4460dfe36d74b
Alexander Negodiaev <alex.negodiaev@gmail.com>
2024-02-19 17:37:10 +0500

    Add Babel and i18n-extract-plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -42,5 +42,22 @@ export function buildLoaders({isDev}: IBuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    return [tsLoader, cssLoader, svgLoader, fileLoader]
+    const babelLoader = {
+        test: /\.(ts?x|js?x)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    ["i18next-extract", {
+                        locales: ['en', 'ru'],
+                        keyAsDefaultValue: true
+                    }]
+                ]
+            }
+        }
+    };
+
+    return [fileLoader, svgLoader, babelLoader, tsLoader, cssLoader]
 }
