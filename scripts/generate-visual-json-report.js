9bdcd7c16a7ba52f26e37a059cec76b59388c8a4
Alexander Negodiaev <alex.negodiaev@gmail.com>
2024-02-29 14:54:55 +0500

    Add a tool for viewing loki screenshots

---
+++ scripts/generate-visual-json-report.js
@@ -0,0 +1,30 @@
+// Display loki's test results
+// https://github.com/oblador/loki/issues/308#issuecomment-789228704
+const { promisify } = require('util');
+const { readdir, writeFile } = require('fs');
+const { join: joinPath, relative } = require('path');
+
+const asyncReaddir = promisify(readdir);
+const writeFileAsync = promisify(writeFile);
+
+const lokiDir = joinPath(__dirname, '..', '.loki');
+const actualDir = joinPath(lokiDir, 'current');
+const expectedDir = joinPath(lokiDir, 'reference');
+const diffDir = joinPath(lokiDir, 'difference');
+
+(async function main() {
+  const diffs = await asyncReaddir(diffDir);
+
+  await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
+    newItems: [],
+    deletedItems: [],
+    passedItems: [],
+    failedItems: diffs,
+    expectedItems: diffs,
+    actualItems: diffs,
+    diffItems: diffs,
+    actualDir: relative(lokiDir, actualDir),
+    expectedDir: relative(lokiDir, expectedDir),
+    diffDir: relative(lokiDir, diffDir),
+  }));
+}());
